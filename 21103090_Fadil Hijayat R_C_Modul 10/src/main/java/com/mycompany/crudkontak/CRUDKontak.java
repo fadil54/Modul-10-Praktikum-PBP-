cb6beac438ae8a9d4e35b07e8bd65066aa42f9f3
adil <fadilhr54@gmail.com>
2023-01-03 22:42:39 +0700

    First commit

---
+++ 21103090_Fadil Hijayat R_C_Modul 10/src/main/java/com/mycompany/crudkontak/CRUDKontak.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.crudkontak;
+import com.mycompany.crudkontak.Frame.FrameKontak;
+import javax.swing.SwingUtilities;
+/**
+ *
+ * @author Acer SPIN
+ */
+public class CRUDKontak {
+
+    public static void main(String[] args) {
+        new FrameKontak().setVisible(true);
+    }
+}
